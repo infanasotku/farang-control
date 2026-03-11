53e70ac6ce3a3ae39318f64e3c1f0f769daf1244
infanasotku <infanasotku@gmail.com>
2026-03-12 00:59:54 +0700

    Add PgSpecUnitOfWork and related context classes for engine specifications

---
+++ app/infra/database/uows/__init__.py
@@ -0,0 +1,8 @@
+from app.infra.database.uows.base import PgUnitOfWork
+from app.infra.database.uows.spec import PgSpecUnitOfWork
+
+__all__ = [
+    "PgUnitOfWork",
+    #
+    "PgSpecUnitOfWork",
+]
---
+++ app/infra/database/uows/spec.py
@@ -0,0 +1,24 @@
+from sqlalchemy.ext.asyncio import AsyncSession, AsyncSessionTransaction
+
+from app.infra.database.repositories.core import PgEngineSpecRepository, PgEngineSpecTxRepository
+from app.infra.database.uows.base import PgTxUOWContext, PgUnitOfWork, PgUOWContext
+
+
+class SpecContext(PgUOWContext):
+    def __init__(self, *, session: AsyncSession):
+        super().__init__(session=session)
+        self.specs = PgEngineSpecRepository(session=session)
+
+
+class SpecTxContext(PgTxUOWContext):
+    def __init__(self, *, session: AsyncSession, transaction: AsyncSessionTransaction):
+        super().__init__(session=session, transaction=transaction)
+        self.specs = PgEngineSpecTxRepository(session=session)
+
+
+class PgSpecUnitOfWork(PgUnitOfWork[SpecContext, SpecTxContext]):
+    def _make_plain_ctx(self, *, session: AsyncSession) -> SpecContext:
+        return SpecContext(session=session)
+
+    def _make_tx_ctx(self, *, session: AsyncSession, transaction: AsyncSessionTransaction) -> SpecTxContext:
+        return SpecTxContext(session=session, transaction=transaction)
