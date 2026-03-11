ed5a4828c9eb260d9cfa6d3b2cf17ec9a525518c
infanasotku <infanasotku@gmail.com>
2026-03-12 01:00:08 +0700

    Add SpecService class with dependency injection setup

--- app/container.py
+++ app/container.py
@@ -1,7 +1,25 @@
 from dependency_injector import containers, providers
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
 from app.infra.config import generate_settings
+from app.infra.database import create_engine
+from app.infra.database.uows import PgSpecUnitOfWork
+from app.services.spec import SpecService
 
 
 class Container(containers.DeclarativeContainer):
     settings = providers.Singleton(generate_settings)
+
+    plain_engine = providers.Singleton(create_engine, settings.provided.postgres, tx=False)
+    tx_engine = providers.Singleton(create_engine, settings.provided.postgres, tx=True)
+
+    plain_sessionmaker = providers.Singleton(async_sessionmaker[AsyncSession], plain_engine)
+    tx_sessionmaker = providers.Singleton(async_sessionmaker[AsyncSession], tx_engine)
+
+    spec_uow = providers.Factory(
+        PgSpecUnitOfWork,
+        plain_sessionmaker=plain_sessionmaker,
+        tx_sessionmaker=tx_sessionmaker,
+    )
+
+    spec_serivce = providers.Factory(SpecService, spec_uow)
---
+++ app/services/spec.py
@@ -0,0 +1,13 @@
+from uuid import UUID
+
+from app.domains.spec import EngineSpec
+from app.infra.database.uows import PgSpecUnitOfWork
+
+
+class SpecService:
+    def __init__(self, uow: PgSpecUnitOfWork) -> None:
+        self._uow = uow
+
+    async def get_by_engine(self, engine_id: UUID) -> EngineSpec | None:
+        async with self._uow.begin(with_tx=False) as ctx:
+            return await ctx.specs.get_engine_spec(engine_id)
